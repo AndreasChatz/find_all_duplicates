9644a67754741c1525f84211a7850431d6b59e7a
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-08-23 17:38:08 +0300

    Add docstring and comments

--- find_all_duplicates.py
+++ find_all_duplicates.py
@@ -1,8 +1,18 @@
 
 def find_all_duplicates(given_array):
+    """
+    Given an array of integers the method returns all the duplicate values of the array as a set.
+    The values of the array must be between 1 and n where n is the length of the array.
+    :param given_array: An array of values from 1 to n (n is the length of the array).
+    :return: A set of all the duplicate values.
+    """
 
+    # create an empty set
     result = set()
 
+    # For every element get the value. Make that value positive, subtract one and use it as index.
+    # If the value of the element at position index is negative, add the value of the current element to result set,
+    # else make the value of the element at the position 'index' negative.
     for i in range(len(given_array)):
         index = abs(given_array[i]) - 1
         if given_array[index] < 0:
